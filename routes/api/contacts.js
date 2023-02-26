473444adaa294ef6f22f60c4696a44620f138fc9
Sergey-Ryabko-84 <amigoswow@ukr.net>
2023-02-26 03:01:09 +0200

    adds work with a collection of contacts in the database

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const ctrl = require("../../controllers/contacts");
-const { validateBody } = require("../../middlewares")
-const schema = require("../../schemas/contacts")
+const { validateBody, isValidId } = require("../../middlewares");
+const {schemas} = require("../../models/contact")
 
 const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
-router.get("/:id", ctrl.getById);
+router.get("/:id", isValidId, ctrl.getById);
 
-router.post("/", validateBody(schema.addSchema), ctrl.add);
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-router.put("/:id", validateBody(schema.putSchema), ctrl.updateById);
+router.put("/:id", isValidId, validateBody(schemas.putSchema), ctrl.updateById);
 
-router.delete("/:id", ctrl.deleteById);
+router.patch(
+  "/:id/favorite",
+  isValidId,
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateStatusContact
+);
+
+router.delete("/:id", isValidId, ctrl.deleteById);
 
 module.exports = router;
